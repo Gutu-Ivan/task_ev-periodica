65d98f8461112bb563849d5d3b6bf77c6a33c8a5
Gutu Ivan <gutsu.ivan@mail.ru>
2021-12-21 12:27:27 +0200

    +Create phone controller

---
+++ src/main/java/dev/task_evperiodica/controllers/PhoneController.java
@@ -0,0 +1,9 @@
+package dev.task_evperiodica.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/phones/")
+public class PhoneController {
+}
