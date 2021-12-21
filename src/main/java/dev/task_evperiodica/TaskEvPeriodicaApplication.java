26efdef24180c3ed459beb241f44175f0a0c2165
Gutu Ivan <gutsu.ivan@mail.ru>
2021-12-21 12:16:17 +0200

    Initial commit

---
+++ src/main/java/dev/task_evperiodica/TaskEvPeriodicaApplication.java
@@ -0,0 +1,13 @@
+package dev.task_evperiodica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskEvPeriodicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskEvPeriodicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/task_evperiodica/TaskEvPeriodicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.task_evperiodica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskEvPeriodicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
